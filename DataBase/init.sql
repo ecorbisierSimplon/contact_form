bba1dd4512fac8dc20add91d55c3db3cf8e1a0f3
Hyeminah <nolwenn.dewasch@hotmail.com>
2023-12-11 14:29:12 +0100

    sqltablecontact

--- DataBase/init.sql
+++ DataBase/init.sql
@@ -1,12 +1,12 @@
 
 CREATE TABLE IF NOT EXISTS users (
-        ID_USER SERIAL,
+        ID_contact SERIAL,
+        subject VARCHAR(255) NOT NULL
         first_name VARCHAR(255) NOT NULL,
         last_name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL,
-        pseudo VARCHAR(255) NOT NULL,
-        key_password VARCHAR(1024) NOT NULL,
-        is_admin BOOLEAN NOT NULL,
+        lmail VARCHAR(255) NOT NULL,
+        phone_number CHAR(20) NOT NULL,
+        message TEXT NOT NULL;
         date_create TIMESTAMP NOT NULL,
         UNIQUE (ID_USER),
         PRIMARY KEY (ID_USER)
