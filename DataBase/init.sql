2a428e4a99ff5f7416869afe898b2e2cc51115fc
Hyeminah <nolwenn.dewasch@hotmail.com>
2023-12-11 14:45:48 +0100

    modif

--- DataBase/init.sql
+++ DataBase/init.sql
@@ -1,13 +1,13 @@
 
 CREATE TABLE IF NOT EXISTS users (
-        ID_contact SERIAL,
+        id_contact SERIAL,
         subject VARCHAR(255) NOT NULL
         first_name VARCHAR(255) NOT NULL,
         last_name VARCHAR(255) NOT NULL,
-        lmail VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
         phone_number CHAR(20) NOT NULL,
-        message TEXT NOT NULL;
+        message VARCHAR() NOT NULL;
         date_create TIMESTAMP NOT NULL,
-        UNIQUE (ID_USER),
-        PRIMARY KEY (ID_USER)
+        UNIQUE (id_contact),
+        PRIMARY KEY (id_contact)
     );
