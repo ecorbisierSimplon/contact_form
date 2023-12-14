ce1df915c27548331e965b90de1fb159e23c9eae
Hyeminah <nolwenn.dewasch@hotmail.com>
2023-12-14 15:02:31 +0100

    send

--- src/services/MessageSend.ts
+++ src/services/MessageSend.ts
@@ -1,13 +1,17 @@
 import { transporter } from "./serveurSMTP"
 
-export async function main() {
+import{ HtmlText} from "../controllers/htmlText";
+
+
+export async function main(subject: string, lastname: string, firstname: string, email: string, phone: string, message: string) {
   // send mail with defined transport object
+  const userMessage :string ="<h2>Contact Form Submission</h2><p><strong>Last Name:</strong> ${lastName}</p><p><strong>First Name:</strong> ${firstName}</p><p><strong>Email:</strong> ${email}</p><p><strong>Phone:</strong> ${phone}</p><h3>Message:</h3><p>${message}</p>";
   const info = await transporter.sendMail({
     from: '"Fred Foo 👻" <ne_pas_repondre@corbisier.fr>', // sender address
     to: "eric@corbisier.fr", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
+    subject: subject, // Subject line
+    text: HtmlText.htmlToPlainText(userMessage), // plain text body
+    html: userMessage, // html body
   });
 
   console.log("Message sent: %s", info.messageId);
