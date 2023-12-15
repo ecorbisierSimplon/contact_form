481aa5290091a320a9204a939351f782f46b8356
eric corbisier <ecorbisier.simplon@gmail.com>
2023-12-15 14:38:18 +0100

    change serveurSMTP

--- src/services/serveurSMTP.ts
+++ src/services/serveurSMTP.ts
@@ -4,11 +4,11 @@ dotenv.config();
 
 const hostname = process.env.SMTP_HOST || "smtp.ionos.fr";
 const username = process.env.SMTP_USERNAME || "ne_pas_repondre@corbisier.fr";
-const password = process.env.SMTP_PASSWORD || "Einstein:2020#";
+const password = process.env.SMTP_PASSWORD || "";
 const port = parseInt(process.env.SMTP_PORT || "465", 10); // then convert the result to an integer using base 10 (decimal system).
 const nodemailer = require("nodemailer");
 
-export const transporter: Transporter = nodemailer.createTransport ({
+export const transporter: Transporter = nodemailer.createTransport({
   host: hostname,
   port: port,
   secure: true,
